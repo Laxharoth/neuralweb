1ef33aba41052b52e6f8fad9b17eb9e8dba7e963
zeyden <zeydenlaxharoth@gmail.com>
2021-03-10 16:53:51 -0600

    -Added abstract class function -Added abstract method evaluate to function class -Added derivative method to function class (returns function pointer) -Added derivative method to function class (returns double) -Added integral method to function class -Overriden operator '()' to call evaluate method -Added child class derivative -Added constructor for derivative class -Added private function pointer to original function -Overriden evaluate method

---
+++ header/function.hpp
@@ -0,0 +1,72 @@
+#ifndef FUNCTION_HPP
+#define FUNCTION_HPP
+
+/*
+*	Is a template of a function
+*/
+class function
+{
+public:
+	function();
+	~function();
+	/*
+	*	Evaluates the function in x
+	*	@param x The point in which the function will be evaluated
+	*	@return The value of "y" with the function evaluated in x
+	*/
+	virtual double evaluate(double x) = 0;
+	/*
+	*	Creates a function which is the derivative of the function
+	*	(the third derivative has very high error)
+	*	@return A function pointer to an aproximation to the derivative of the function
+	*/
+	function* derivative();
+	/*
+	*	Finds the slope in the function at a given point x
+	*	@param x The point in which the slope will be calculated
+	*	@return An aproximation of the slope of the function
+	*/
+	double derivative(double x);
+	/*
+	*	Evaluates the defined Integral (aproximation) of the function from a to b
+	*	(is very slow)
+	*	@param a The starting point in the defined integral
+	*	@param b The end point in the defined integral 
+	*/
+	double integral(double a, double b);
+	/*
+	*	Overloads the operator () to evaluate the function in x
+	*	@param x The point in which the function will be evaluated
+	*	@return The value of "y" with the function evaluated in x
+	*/
+	double operator()(double x);
+protected:
+	//A very small number (limit t->0)
+	static const double h;
+private:
+};
+/*
+*	Is a template of a derivative
+*/
+class derivative_function : public function
+{
+	public:
+		/*
+		*	Creates an object which is the derivative(aproximation)
+		*	of another function (is possible to get the derivatives derivative)
+		*	@param outer_function Is the original function
+		*	@return A function being the derivative of the original function
+		*/
+		derivative_function(function* outer_function);
+		/*
+		*	Gets the slope of the original function at a given point x
+		*	@param x The point in which the slope will be calculated
+		*	@return An aproximation of the slope of the function
+		*/
+		virtual double evaluate(double x);
+	private:
+		//The original function
+		function* outer_function;
+};
+
+#endif // FUNCTION_HPP
---
+++ src/function.cpp
@@ -0,0 +1,70 @@
+#include "function.hpp"
+
+//A very small number (limit t->0)
+const double function::h = 0.000001f;
+
+function::function(){};
+function::~function(){};
+
+/*
+*	Creates a function which is the derivative of the function
+*	(the third derivative has very high error)
+*	@return A function pointer to an aproximation to the derivative of the function
+*/
+function* function::derivative()
+{
+	return new derivative_function(this);
+}
+
+/*
+*	Finds the slope in the function at a given point x
+*	@param x The point in which the slope will be calculated
+*	@return An aproximation of the slope of the function
+*/
+double function::derivative(double x){
+	return ((this->evaluate(x+h)-this->evaluate(x))/h);
+}
+
+/*
+*	Evaluates the defined Integral (aproximation) of the function from a to b
+*	(is very slow)
+*	@param a The starting point in the defined integral
+*	@param b The end point in the defined integral 
+*/
+double function::integral(double a, double b){
+	double sum = 0;
+	double first = (a<b)? a:b;
+	double second=(a<b)? b:a;
+	for (double i =first; first <= second; first+=h)
+		sum += this->evaluate(i)*h;
+	if(a>b)sum*=-1;
+	return sum;
+}
+
+/*
+*	Overloads the operator () to evaluate the function in x
+*	@param x The point in which the function will be evaluated
+*	@return The value of "y" with the function evaluated in x
+*/
+double function::operator()(double x){
+	return this->evaluate(x);
+}
+
+/*
+*	Creates an object which is the derivative(aproximation)
+*	of another function (is possible to get the derivatives derivative)
+*	@param outer_function Is the original function
+*	@return A function being the derivative of the original function
+*/
+derivative_function::derivative_function(function* outer_function):
+outer_function(outer_function){};
+
+/*
+*	Gets the slope of the original function at a given point x
+*	@param x The point in which the slope will be calculated
+*	@return An aproximation of the slope of the function
+*/
+double derivative_function::evaluate(double x)
+{
+	return ((this->outer_function->evaluate(x+h)-this->outer_function->evaluate(x))/h);
+}
