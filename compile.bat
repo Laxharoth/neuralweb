acada19ca1ec82443146f7a6e3a39495e53791b7
zeyden <zeydenlaxharoth@gmail.com>
2021-03-17 15:54:03 -0600

    Added backpropagation to compilation

--- compile.bat
+++ compile.bat
@@ -1,8 +1,8 @@
 ::c++ -c .\src\function.cpp .\test.cpp -I.\header
 ::c++ .\test.o  .\function.o -o main.exe
 ::DEBUGGER
-::g++  .\src\neuron.cpp .\src\layer.cpp .\src\web.cpp .\src\function.cpp .\src\web_file_handle.cpp .\test_web.cpp -I.\header
-c++ -c  .\src\neuron.cpp .\src\layer.cpp .\src\web.cpp .\src\function.cpp .\src\web_file_handle.cpp .\test_web.cpp -I.\header
-c++ neuron.o layer.o web.o function.o web_file_handle.o test_web.o  -o main.exe
+::g++  .\src\neuron.cpp .\src\layer.cpp .\src\web.cpp .\src\function.cpp .\src\web_file_handle.cpp .\src\backpropagation.cpp .\test_web.cpp -I.\header
+c++ -c  .\src\neuron.cpp .\src\layer.cpp .\src\web.cpp .\src\function.cpp .\src\web_file_handle.cpp .\src\backpropagation.cpp .\test_web.cpp -I.\header
+c++ neuron.o layer.o web.o function.o web_file_handle.o backpropagation.o test_web.o -o main.exe
 del *.o
-.\main.exe
+::.\main.exe
